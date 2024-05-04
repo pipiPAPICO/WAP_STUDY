abcb07915237f762fdb6810cb77b9be94bf2f1d5
pipiPAPICO <163659664+pipiPAPICO@users.noreply.github.com>
2024-05-05 03:13:38 +0900

    유니티 프로젝트 업로드
    프로젝트를 이식했습니다.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public Dongle lastDongle;
+    public GameObject donglePrefab;
+    public Transform dongleGroup;
+
+    private void Awake()
+    {
+        Application.targetFrameRate = 60;
+    }
+    void Start()
+    {
+        NextDongle();
+    }
+    Dongle GetDongle()
+    {
+        GameObject instant = Instantiate(donglePrefab, dongleGroup);
+        Dongle instantDongle = instant.GetComponent<Dongle>();
+       return instantDongle;
+    }
+    void NextDongle()
+    {
+        Dongle newDongle = GetDongle();
+        lastDongle = newDongle;
+        lastDongle.level = Random.Range(0, 8); // MAX값은 포함안됨
+        lastDongle.gameObject.SetActive(true);
+        StartCoroutine("WaitNext"); // 함수 이름 그대로 or 문자열로
+    }
+    IEnumerator WaitNext()
+    {
+        while (lastDongle != null)
+        {
+            yield return null;
+        }
+        yield return new WaitForSeconds(2.5f);
+
+        NextDongle();
+    }
+    public void TouchDown()
+    {
+        if (lastDongle == null)
+            return;
+
+        lastDongle.Drag();
+    }
+    public void TouchUp()
+    {
+        if (lastDongle == null)
+            return;
+
+        lastDongle.Drop();
+        lastDongle = null;
+    }
+}
