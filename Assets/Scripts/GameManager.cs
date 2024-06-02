305a6f20e54d886aa5de7d27d081c1d38fb8af8e
pipiPAPICO <163659664+pipiPAPICO@users.noreply.github.com>
2024-06-02 19:07:59 +0900

    유니티 프로젝트 업로드
    이식 완료

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -10,6 +10,12 @@ public class GameManager : MonoBehaviour
     public GameObject effectPrefab;
     public Transform effectGroup;
 
+    public AudioSource bgmPlayer;
+    public AudioSource[] sfxPlayer;
+    public AudioClip[] sfxClip;
+    public enum Sfx { LevelUp, Next, Attach, Button, Over };
+
+    int sfxCursor; // sfxPlayer 배열의 원소를 가리키는 커서
 
     public int score;
     public int maxLevel;
@@ -20,6 +26,7 @@ public class GameManager : MonoBehaviour
     }
     void Start()
     {
+        bgmPlayer.Play();
         NextDongle();
     }
     Dongle GetDongle()
@@ -45,6 +52,8 @@ public class GameManager : MonoBehaviour
         lastDongle.manager = this;
         lastDongle.level = Random.Range(0, maxLevel); // MAX값은 포함안됨
         lastDongle.gameObject.SetActive(true);
+
+        SfxPlay(Sfx.Next);
         StartCoroutine("WaitNext"); // 함수 이름 그대로 or 문자열로
     }
     IEnumerator WaitNext()
@@ -98,5 +107,32 @@ public class GameManager : MonoBehaviour
             dongles[index].Hide(Vector3.up * 100);
             yield return new WaitForSeconds(0.1f);
         }
+
+        yield return new WaitForSeconds(1f);
+
+        SfxPlay(Sfx.Over);
+    }
+    public void SfxPlay(Sfx type)
+    {
+        switch (type)
+        {
+            case Sfx.LevelUp:
+                sfxPlayer[sfxCursor].clip = sfxClip[Random.Range(0, 3)];
+                break;
+            case Sfx.Next:
+                sfxPlayer[sfxCursor].clip = sfxClip[3];
+                break;
+            case Sfx.Attach:
+                sfxPlayer[sfxCursor].clip = sfxClip[4];
+                break;
+            case Sfx.Button:
+                sfxPlayer[sfxCursor].clip = sfxClip[5];
+                break;
+            case Sfx.Over:
+                sfxPlayer[sfxCursor].clip = sfxClip[6];
+                break;
+        }
+        sfxPlayer[sfxCursor].Play();
+        sfxCursor = (sfxCursor + 1) % sfxPlayer.Length; // OutOfRange 방지
     }
 }
